20c027976b3f09574b581881393b7588ba81b685
josue <josueeliashuaman@gmail.com>
2024-11-04 03:14:47 -0500

    feat: check overflow for A bus

--- new_scope_josue/fede_idea/obj.py
+++ new_scope_josue/fede_idea/obj.py
@@ -49,88 +49,104 @@ def obj_plus_obj(a_obj, b_obj):
         print("objs doesn't have the same phase")
 
 
-def check_overflow(objs):
+def check_overflow(objs, bus):
 
     # you are only checking overflow in bus O, requires analysis over buses A,D,B,C
-    # rename to overflow
-    for obj in objs:
-        if conf_file.buses_metadata["O"]["width"] < obj["MSB_position"]:
-            return {"overflow": True, "log": f"greatter that bus O"}
+    if bus == "A":
+        for obj in objs:
+            if conf_file.buses_metadata[bus]["width"] < obj["MSB_position"]:
+                return {
+                    "overflow": True,
+                    "log": f"Overflow in bus {bus}: greatter that bus {bus} width",
+                }
+        return {"overflow": False, "log": f"No overflow in bus {bus}"}
 
-    for n, obj1 in enumerate(objs):
-        for obj2 in objs[n + 1 :]:
-            """
-            L = LSB
-            M = MSB
+    if bus == "O":
+        for obj in objs:
+            if conf_file.buses_metadata[bus]["width"] < obj["MSB_position"]:
+                return {
+                    "overflow": True,
+                    "log": f"Overflow in bus {bus}: greatter that bus {bus} width",
+                }
 
-            case 1
-                |-------|       obj1
+        for n, obj1 in enumerate(objs):
+            for obj2 in objs[n + 1 :]:
+                """
+                L = LSB
+                M = MSB
+
+                case 1
+                    |-------|       obj1
+                    M       L
+                        |-------|   obj2
+                        M       L
+
+                case 2
+                    |-------|       obj1
+                    M       L
+                |-------|           obj2
                 M       L
-                    |-------|   obj2
+
+                case 3
+                    |-------|       obj1
+                    M       L
+                |---------------|   obj2
+                M               L
+
+                case 4
+                |---------------|   obj1
+                M               L
+                    |-------|       obj2
                     M       L
 
-            case 2
-                |-------|       obj1
-                M       L
-            |-------|           obj2
-            M       L
-
-            case 3
-                |-------|       obj1
-                M       L
-            |---------------|   obj2
-            M               L
-
-            case 4
-            |---------------|   obj1
-            M               L
-                |-------|       obj2
+                case 5
+                        |-------|   obj1
+                        M       L
+                |-------|           obj2
                 M       L
 
-            case 5
-                    |-------|   obj1
-                    M       L
-            |-------|           obj2
-            M       L
+                case 6
+                |-------|           obj1
+                M       L
+                        |-------|   obj2
+                        M       L
+                """
 
-            case 6
-            |-------|           obj1
-            M       L
-                    |-------|   obj2
-                    M       L
-            """
+                # case 1
+                if (
+                    obj2["LSB_position"] < obj1["LSB_position"] < obj2["MSB_position"]
+                    and obj2["MSB_position"] < obj1["MSB_position"]
+                ):
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 1"}
+                # case 2
+                if (
+                    obj2["LSB_position"] < obj1["MSB_position"] < obj2["MSB_position"]
+                    and obj1["LSB_position"] < obj2["LSB_position"]
+                ):
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 2"}
+                # case 3
+                if (
+                    obj2["LSB_position"] < obj1["LSB_position"] < obj2["MSB_position"]
+                    and obj2["LSB_position"]
+                    < obj1["MSB_position"]
+                    < obj2["MSB_position"]
+                ):
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 3"}
+                # case 4
+                if (
+                    obj1["LSB_position"] < obj2["LSB_position"] < obj1["MSB_position"]
+                    and obj1["LSB_position"]
+                    < obj2["MSB_position"]
+                    < obj1["MSB_position"]
+                ):
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 4"}
 
-            # case 1
-            if (
-                obj2["LSB_position"] < obj1["LSB_position"] < obj2["MSB_position"]
-                and obj2["MSB_position"] < obj1["MSB_position"]
-            ):
-                return {"overflow": True, "log": f"case 1"}
-            # case 2
-            if (
-                obj2["LSB_position"] < obj1["MSB_position"] < obj2["MSB_position"]
-                and obj1["LSB_position"] < obj2["LSB_position"]
-            ):
-                return {"overflow": True, "log": f"case 2"}
-            # case 3
-            if (
-                obj2["LSB_position"] < obj1["LSB_position"] < obj2["MSB_position"]
-                and obj2["LSB_position"] < obj1["MSB_position"] < obj2["MSB_position"]
-            ):
-                return {"overflow": True, "log": f"case 3"}
-            # case 4
-            if (
-                obj1["LSB_position"] < obj2["LSB_position"] < obj1["MSB_position"]
-                and obj1["LSB_position"] < obj2["MSB_position"] < obj1["MSB_position"]
-            ):
-                return {"overflow": True, "log": f"case 4"}
+                # case 5
+                if obj1["MSB_position"] == obj2["LSB_position"]:
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 5"}
 
-            # case 5
-            if obj1["MSB_position"] == obj2["LSB_position"]:
-                return {"overflow": True, "log": f"case 5"}
+                # case 6
+                if obj1["LSB_position"] == obj2["MSB_position"]:
+                    return {"overflow": True, "log": f"Overflow in bus {bus}: case 6"}
 
-            # case 6
-            if obj1["LSB_position"] == obj2["MSB_position"]:
-                return {"overflow": True, "log": f"case 6"}
-
-    return {"overflow": False, "log": f"no overflow"}
+        return {"overflow": False, "log": f"No overflow in bus {bus}"}
