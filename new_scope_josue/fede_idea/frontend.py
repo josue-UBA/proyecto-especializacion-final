904bc3cd9c034239e487ec2d4459e3cdc9338ad7
josue <josueeliashuaman@gmail.com>
2024-11-03 22:39:59 -0500

    feat: add data to frontend

--- new_scope_josue/fede_idea/frontend.py
+++ new_scope_josue/fede_idea/frontend.py
@@ -14,29 +14,30 @@ def print_analysis(name, A_bus, D_bus, B_bus, C_bus, buses, log, O_base_objs):
     ] + ["|"]
     c = " ".join(b)[::-1]
 
-
-    # Only string that grow dinamically use :>
+    # Only string that grow dinamically use ":>"
     text = f"""
-=========================================================================================================================
-                                        configuration: {name}
-=========================================================================================================================
+=============================================================================================================================
+                                            configuration: {name} over DSP48E1
+=============================================================================================================================
+   
+                 4 4 4 4 4 4 4 4 4 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+ bit position => 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1
+                                                     {A_bus}
+                                                                                        +
+                                                               {D_bus}
+                                                                                        x
+                                                                             {B_bus}
+                                                                                        +
+                 {C_bus}
+                                                                                        =
+range of numb => {c}
 
-             4 4 4 4 4 4 4 4 4 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
-             8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1
-                                                 {A_bus}
-                                                                                    +
-                                                       {D_bus}
-                                                                                    x
-                                                                         {B_bus}
-                                                                                    +
-             {C_bus}
-                                                                                    =
-             {c}  <= limit of numbers
-{chr(10).join([f'{text:>{conf_file.LEFT_SPACE}}' for text in [f'iteration {n} {bus}' for n,bus in enumerate(buses)]])}
-             
+{chr(10).join([f'{text:>{conf_file.LEFT_SPACE}}' for text in [f'iteration {n} => {bus}' for n,bus in enumerate(buses)]])}
 
+range of numb => {c}
+
+   
 overflow occurs: {log}
 
 """
     print(text)
-
