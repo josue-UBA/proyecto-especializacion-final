feb03b54b19456a2267fcfb1220af9b6925577ac
josue <josueeliashuaman@gmail.com>
2024-11-03 20:43:29 -0500

    feat: optimizing frontend

--- new_scope_josue/fede_idea/frontend.py
+++ new_scope_josue/fede_idea/frontend.py
@@ -6,25 +6,6 @@ def print_log(log):
 
 
 def print_analysis(name, A_bus, D_bus, B_bus, C_bus, buses, log, O_base_objs):
-
-    print("==============================================")
-    print(f"{name}")
-    print("==============================================\n")
-
-    guia1 = "4 4 4 4 4 4 4 4 4 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1                               "
-    guia2 = "8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1             "
-    print(f"{guia1:>{conf_file.LEFT_SPACE}}")
-    print(f"{guia2:>{conf_file.LEFT_SPACE}}")
-
-    print(f"{A_bus:>{conf_file.LEFT_SPACE}}")
-    print("\t\t\t\t\t\t\t\t\t\t\t\t+")
-    print(f"{D_bus:>{conf_file.LEFT_SPACE}}")
-    print("\t\t\t\t\t\t\t\t\t\t\t\tx")
-    print(f"{B_bus:>{conf_file.LEFT_SPACE}}")
-    print("\t\t\t\t\t\t\t\t\t\t\t\t+")
-    print(f"{C_bus:>{conf_file.LEFT_SPACE}}")
-    print("\t\t\t\t\t\t\t\t\t\t\t\t=")
-
     a = [i["LSB_position"] for i in O_base_objs] + [
         i["LSB_position"] - 1 for i in O_base_objs
     ]
@@ -33,9 +14,29 @@ def print_analysis(name, A_bus, D_bus, B_bus, C_bus, buses, log, O_base_objs):
     ] + ["|"]
     c = " ".join(b)[::-1]
 
-    print(f"{c:>{conf_file.LEFT_SPACE-13}}")
-    for n, i in enumerate(buses):
-        itera = f"iteration {n} {i}"
-        print(f"{itera:>{conf_file.LEFT_SPACE}}")
-    print()
-    print(log)
+
+    # Only string that grow dinamically use :>
+    text = f"""
+=========================================================================================================================
+                                        configuration: {name}
+=========================================================================================================================
+
+             4 4 4 4 4 4 4 4 4 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+             8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1
+                                                 {A_bus}
+                                                                                    +
+                                                       {D_bus}
+                                                                                    x
+                                                                         {B_bus}
+                                                                                    +
+             {C_bus}
+                                                                                    =
+             {c}  <= limit of numbers
+{chr(10).join([f'{text:>{conf_file.LEFT_SPACE}}' for text in [f'iteration {n} {bus}' for n,bus in enumerate(buses)]])}
+             
+
+overflow occurs: {log}
+
+"""
+    print(text)
+
